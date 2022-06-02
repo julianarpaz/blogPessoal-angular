32ac89b579f72e5a1be98dd48ca7f97c5584fa2e
julianarpaz <julianarpaz@gmail.com>
2022-06-02 16:05:13 -0300

    Criação do componente início

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -19,4 +20,12 @@ export class AuthService {
     return this.http.post<Usuario>('https://blogjulianapaz.herokuapp.com/usuarios/cadastrar', usuario)
   }
   //acessar um endereço externo(heroku) e utilizar algum método HTTP
+
+  logado(){
+    let ok: boolean = false
+    if(environment.token != ''){
+      ok = true
+    }
+    return ok
+  }
 }
